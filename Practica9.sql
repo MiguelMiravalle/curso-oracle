4b3d1710bca765250622a36acf2286b4ad36f76b
Miguel Angel Miravalle Vazquez <miguelmiravalle@hotmail.com>
2017-03-11 09:25:17 -0600

    Update Practica9.sql

--- Practica9.sql
+++ Practica9.sql
@@ -9,3 +9,19 @@ RAISE_APPLICATION_ERROR(-20001,'Eres menor de edad');
 END IF;
 END;
 / 
+
+create table usuario_xxx(
+id integer,
+nombre varchar2(40),
+edad integer,
+constraint PK_id primary key (id)
+)
+/
+create or replace trigger disp_usuario_xxx before insert on usuario_xxx
+for each row
+begin
+if :NEW.edad < 18 then 
+raise_application_error(-200001,'estas chavo');
+end if;
+end;
+/
